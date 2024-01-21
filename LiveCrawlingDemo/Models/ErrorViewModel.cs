fd563727f66c3987e3d88d0231d11a8aa1a4c8af
LuongAnh <luonganh@gmail.com>
2024-01-21 18:05:07 +0700

    Add project files.

---
+++ LiveCrawlingDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LiveCrawlingDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
